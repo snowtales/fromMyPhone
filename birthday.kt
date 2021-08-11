a7025264ac056147e6a327fbfe55e135a1b0dfb5
kashpirovich <snowtales@list.ru>
2021-08-11 21:44:40 +0000

    testing again from my smartphone

--- birthday.kt
+++ birthday.kt
@@ -1,13 +1,32 @@
 fun main(){
-	var name = readLine()!!
+	var (name, age) = readLine()!!.split(" ").map{it}
 	val border = "'-._,-'"
 	val times = 4
+	val layers = 5
 	printBorder(border, times)
-	println("    Happy birthday, ${name}!")
+	println("  Happy birthday, ${name}!")
 	printBorder(border, times)
+	printCake(age.toInt(), layers)
 }
 
 fun printBorder(s: String, i:Int){
 	repeat(i){ print(s)}
 	println()
+}
+fun printCake(age: Int, layers: Int){
+	print(" ")
+	repeat(age){print(",")}
+	println()
+	print(" ")
+	repeat(age){print("|")}
+	println()
+	repeat(age+2){print("=")}
+	println()
+	repeat(layers){
+repeat(age+2){print("@")}
+println()
+}
+println()
+	
+
 }
